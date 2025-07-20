3e5fbffb2b805dcb9c638ffe20167139b8d1ef12
Lugo <hf.lhugo1@gmail.com>
2025-07-20 02:04:49 -0300

    Corrección conexión PostgreSQL y limpieza de rutas duplicadas

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,19 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/usuarios', usuariosRoutes);
 
+// Rutas
+app.get('/api/usuarios', async (req, res) => {
+  const pool = require('./db'); // usa el archivo que ya creaste
+
+  try {
+    const result = await pool.query('SELECT * FROM usuarios');
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener los usuarios' });
+  }
+});
+
 
 app.listen(4000, () => {
   console.log('Servidor backend corriendo en http://localhost:4000')
