ffe032e7afbf457d1aedc070b1c015b28febd2c4
Lugo <hf.lhugo1@gmail.com>
2025-07-19 22:38:44 -0300

    Usa DATABASE_URL y SSL para conexión PostgreSQL en Render

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // 👈 necesario para que funcione con Railway
+    rejectUnauthorized: false
   }
 });
 
