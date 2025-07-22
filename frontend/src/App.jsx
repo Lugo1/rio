6204097d2032df0a212a16deb95b88654ad60308
Lugo <hf.lhugo1@gmail.com>
2025-07-22 14:22:03 -0300

    fix: corregido API_URL para producción

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from 'react';
 //const API_URL = import.meta.env.VITE_API_URL;
 // 🧠 Esto se adapta a local o producción automáticamente
-const API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:4000"
-    : import.meta.env.VITE_API_URL;
-
+const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
 
 
 function App() {
